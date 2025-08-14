8a48898bf38c57c75720939763af24e5adac8970
Jiahui Jiang <jiahui.jiangg@gmail.com>
2025-08-14 16:34:00 -0700

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,27 @@
-'use client';
-import { useEffect } from 'react';
+"use client";
+import RequireAuth from '../../components/RequireAuth';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '../components/AuthProvider';
 
 export default function Home() {
   const { user, loading } = useAuth();
   const router = useRouter();
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if (!loading) {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted && !loading) {
       if (user) {
         router.replace('/members');
       } else {
         router.replace('/login');
       }
     }
-  }, [user, loading, router]);
+  }, [user, loading, router, isMounted]);
 
   return null;
 }
